5d63c96f932be7e172c26a3fa99531e3bedf7140
opigon1 <step.rindin@yandex.ru>
2024-03-05 18:49:31 +0300

    Feature: Реализовал увеличение и уменьшение счётчика

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,10 +1,21 @@
+import React from 'react';
 
 function Counter() {
+  let [counter, setCounter] = React.useState(0);
+
+  function increasingTheCounter() {
+    setCounter((counter += 1));
+  }
+
+  function reducingTheCounter() {
+    setCounter((counter -= 1));
+  }
+
   return (
     <div className='counter'>
-      <button>-</button>
-      <p></p>
-      <button>+</button>
+      <button onClick={reducingTheCounter}>-</button>
+      <p>{counter}</p>
+      <button onClick={increasingTheCounter}>+</button>
     </div>
   );
 }
