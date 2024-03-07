4b731b1f87b43246a532f5650f09e81067b7db1b
opigon1 <step.rindin@yandex.ru>
2024-03-07 11:27:35 +0300

    Feature: Вместо span между кнопками располагает input[type=text]

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,16 +1,20 @@
 import React from 'react';
 
-function Counter() {
+function Counter({ max, min }) {
   let [counter, setCounter] = React.useState(0);
 
+  function onChange(e) {
+    setCounter(Number(e.target.value));
+  }
+
   function increasingTheCounter() {
-    if (counter >= 99) {
+    if (counter >= max) {
       setCounter(99);
     } else setCounter((counter += 1));
   }
 
   function reducingTheCounter() {
-    if (counter <= 0) {
+    if (counter <= min) {
       setCounter(0);
     } else setCounter((counter -= 1));
   }
@@ -18,7 +22,8 @@ function Counter() {
   return (
     <div className='counter'>
       <button onClick={reducingTheCounter}>-</button>
-      <p>{counter}</p>
+      <input type='text' value={counter} onChange={onChange} maxLength={2} />
+      {/* <p>{counter}</p> */}
       <button onClick={increasingTheCounter}>+</button>
     </div>
   );
