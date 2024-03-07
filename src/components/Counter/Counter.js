8153edfa1ba8cbf23a9bfd4005e8a7ce92306b91
opigon1 <step.rindin@yandex.ru>
2024-03-07 13:00:50 +0300

    Feature: Добавил простую анимацию

--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useRef, useState, useEffect } from 'react';
+import './Counter.css'; // Подключаем файл стилей
 
 function Counter({ max, min }) {
-  let [counter, setCounter] = React.useState(0);
+  const [counter, setCounter] = useState(0);
+  const counterRef = useRef();
 
   function onChange(e) {
     setCounter(Number(e.target.value));
@@ -10,13 +12,25 @@ function Counter({ max, min }) {
   function increasingTheCounter() {
     if (counter >= max) {
       setCounter(99);
-    } else setCounter((counter += 1));
+    } else {
+      counterRef.current.classList.add('counter__input_animate_increase');
+      setCounter((prevCounter) => prevCounter + 1);
+      setTimeout(() => {
+        counterRef.current.classList.remove('counter__input_animate_increase');
+      }, 300); // Длительность анимации
+    }
   }
 
   function reducingTheCounter() {
     if (counter <= min) {
       setCounter(0);
-    } else setCounter((counter -= 1));
+    } else {
+      setCounter((prevCounter) => prevCounter - 1);
+      counterRef.current.classList.add('counter__input_animate_decrease');
+      setTimeout(() => {
+        counterRef.current.classList.remove('counter__input_animate_decrease');
+      }, 300); // Длительность анимации
+    }
   }
 
   return (
@@ -31,6 +45,7 @@ function Counter({ max, min }) {
           value={counter}
           onChange={onChange}
           maxLength={2}
+          ref={counterRef}
         />
       </div>
       <button className='counter__btn' onClick={increasingTheCounter}>
